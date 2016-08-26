ff87d17518455d030efcb747fb2471fe699761d5
Aman Gupta <the.alteria@yahoo.com>
2016-08-26 21:52:29 +0530

    update

--- app/src/main/java/com/example/user/muxic/MainActivity2.java
+++ app/src/main/java/com/example/user/muxic/MainActivity2.java
@@ -4,43 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.GridView;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.widget.ImageView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private int PICK_IMAGE_REQUEST = 2;
     ImageView viewImage;
-    Button b;
+    TextView b;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main2);
-        Button switchButton= (Button)findViewById(R.id.ok);
+        TextView switchButton= (TextView)findViewById(R.id.ok);
         switchButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -50,7 +41,14 @@ public class MainActivity2 extends AppCompatActivity {
 
             }
         });
-        b=(Button)findViewById(R.id.btnSelectPhoto);
+        TextView endButton= (TextView)findViewById(R.id.cancel);
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        b=(TextView)findViewById(R.id.btnSelectPhoto);
         viewImage=(ImageView)findViewById(R.id.viewImage);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +56,9 @@ public class MainActivity2 extends AppCompatActivity {
                 selectImage();
             }
         });
-    }
+
+         }
+
 
     private void selectImage() {
 
@@ -74,7 +74,10 @@ public class MainActivity2 extends AppCompatActivity {
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
                     startActivityForResult(intent, 1);
                 } else if (options[item].equals("Choose from Gallery")) {
-                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                   startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
                     startActivityForResult(intent, 2);
 
                 } else if (options[item].equals("Cancel")) {
@@ -109,38 +112,35 @@ public class MainActivity2 extends AppCompatActivity {
                     String path = android.os.Environment
                             .getExternalStorageDirectory()
                             + File.separator
-                            + "Phoenix" + File.separator + "default";
+                            + "Pics" + File.separator + "default";
                     f.delete();
                     OutputStream outFile = null;
-                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    File file = new File(path, "profile" + ".jpg");
                     try {
                         outFile = new FileOutputStream(file);
                         bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
                         outFile.flush();
                         outFile.close();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            } else if (requestCode == 2) {
+            } else if (requestCode == 2 && requestCode == PICK_IMAGE_REQUEST && data != null && data.getData() != null) {
 
-                Uri selectedImage = data.getData();
-                String[] filePath = { MediaStore.Images.Media.DATA };
-                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
-                c.moveToFirst();
-                int columnIndex = c.getColumnIndex(filePath[0]);
-                String picturePath = c.getString(columnIndex);
-                c.close();
-                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-                Log.w("path of image from galler", picturePath+"");
-                viewImage.setImageBitmap(thumbnail);
+                Uri uri = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                    ImageView imageView = (ImageView) findViewById(R.id.viewImage);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-    }
-}
+            }
+        }
+    
--- app/src/main/java/com/example/user/muxic/MainActivity3.java
+++ app/src/main/java/com/example/user/muxic/MainActivity3.java
@@ -1,43 +1,38 @@
 package com.example.user.muxic;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.database.Cursor;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity3 extends AppCompatActivity {
-
-    ListView list;
-
-    int[] images = {R.drawable.bale,
-            R.drawable.ic_attach_file_black_48dp,
-            R.drawable.ic_border_all_black_48dp,
-            R.drawable.ic_format_align_justify_black_48dp,
-            R.drawable.ic_insert_chart_black_48dp,
-            R.drawable.ic_border_outer_black_48dp,
-            R.drawable.ic_format_size_black_48dp};
-
-    String[] titles = {"Aman Gupta","Attachment","Window","Stripes","Bars","Dots","Font"};
-
-    String[] Tabs = {"CALLS","CHATS","CONTACTS"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        Button switchButton= (Button)findViewById(R.id.mn);
-        switchButton.setOnClickListener(new View.OnClickListener() {
+        TextView switchtxt = (TextView) findViewById(R.id.mn);
+        switchtxt.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -46,13 +41,6 @@ public class MainActivity3 extends AppCompatActivity {
 
             }
         });
-
-        list = (ListView) findViewById(R.id.list);
-        myadapter adapter = new myadapter(getApplicationContext(),titles,images);
-        list.setAdapter((ListAdapter) adapter);
-
-
-
 
 
     }
